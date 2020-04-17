27ae4c9dc4833b7ff99afdd9e5320555fa7bfb6e
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-17 08:47:41 -0400

    second commit

---
+++ src/SmallestAndLargestNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SmallestAndLargestNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1= 25;
+        int num2 = 78;
+        int num3 = 87;
+
+        System.out.print("Input the 1st number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int num2 = in.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int num3 = in.nextInt();
+
+
+
+    }
+}
